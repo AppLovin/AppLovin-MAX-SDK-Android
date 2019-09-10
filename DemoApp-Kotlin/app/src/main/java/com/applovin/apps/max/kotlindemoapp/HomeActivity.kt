d2befd21bbb6cd268d1b025ced8fa108e55bbd78
Santosh Bagadi <santosh.bagadi@applovin.com>
2019-09-10 15:45:59 -0700

    Initial commit of Kotlin Demo App

---
+++ DemoApp-Kotlin/app/src/main/java/com/applovin/apps/max/kotlindemoapp/HomeActivity.kt
@@ -0,0 +1,17 @@
+package com.applovin.apps.max.kotlindemoapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+    }
+
+}
