f011b2044c67ae38fd4e64ba23672ea676d9b540
Andrew Tian <tiandrew@umich.edu>
2019-10-18 11:47:35 -0700

    feat/app_icons_and_splash (#6)
    * Update .gitignore
    
    * Splash screen
    
    * App Icons
    
    * Update AndroidManifest.xml
    
    * Formatting
    
    * Remove setHtml

---
+++ DemoApp-Java/app/src/main/java/com/applovin/enterprise/apps/demoapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.applovin.enterprise.apps.demoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.applovin.sdk.AppLovinSdkUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity
+        extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_splash );
+
+        AppLovinSdkUtils.runOnUiThreadDelayed( () -> {
+            Intent intent = new Intent( SplashActivity.this, MainActivity.class );
+            intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION );
+            startActivity( intent );
+        }, TimeUnit.SECONDS.toMillis( 2 ) );
+    }
+}
---
+++ DemoApp-Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.applovin.enterprise.apps.demoapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.applovin.enterprise.apps.demoapp.kotlin.R
+import com.applovin.sdk.AppLovinSdkUtils
+import java.util.concurrent.TimeUnit
+
+class SplashActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        AppLovinSdkUtils.runOnUiThreadDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }, TimeUnit.SECONDS.toMillis(2))
+    }
+}
