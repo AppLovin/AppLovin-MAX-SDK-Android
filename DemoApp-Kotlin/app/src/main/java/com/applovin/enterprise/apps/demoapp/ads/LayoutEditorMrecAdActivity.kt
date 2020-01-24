9c6d444d834cd35b3be076ee46a973e6c3cface2
Andrew Tian <tiandrew@umich.edu>
2020-01-24 13:53:08 -0800

    Feat/max_mrec_support (#10)
    * attrs/strings/androidmanifest
    
    * New files
    
    * Nit
    
    * Remove redundant cast
    
    * Autoformat
    
    * Camelcase bc kotlin magic
    
    * Fix constraints
    
    * MainActivity and xml
    
    * kotlin programmatic mrecs
    
    * comment
    
    * comment
    
    * Rename MRecs->MRECs
    
    * More renaming
    
    * Comment
    
    * Update ProgrammaticMrecAdActivity.kt
    
    * Update ProgrammaticMrecAdActivity.kt
    
    * Delete ProgrammaticMRecAdActivity.java
    
    * Comment

---
+++ DemoApp-Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/LayoutEditorMrecAdActivity.kt
@@ -0,0 +1,51 @@
+package com.applovin.enterprise.apps.demoapp.ads
+
+import android.os.Bundle
+import com.applovin.enterprise.apps.demoapp.kotlin.R
+import com.applovin.enterprise.apps.demoapp.ui.BaseAdActivity
+import com.applovin.mediation.MaxAd
+import com.applovin.mediation.MaxAdViewAdListener
+import kotlinx.android.synthetic.main.activity_layout_editor_mrec_ad.*
+
+/**
+ * [android.app.Activity] used to show AppLovin MAX MREC ads created in the Layout Editor.
+ * <p>
+ * Created by Andrew Tian on 2020-01-14.
+ */
+class LayoutEditorMrecAdActivity : BaseAdActivity(),
+    MaxAdViewAdListener
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_editor_mrec_ad)
+        setTitle(R.string.activity_layout_editor_mrecs)
+
+        setupCallbacksRecyclerView()
+
+        mrecAdView.setListener(this)
+
+        // Load the first ad.
+        mrecAdView.loadAd()
+    }
+
+    //region MAX Ad Listener
+
+    override fun onAdLoaded(ad: MaxAd?) { logCallback() }
+
+    override fun onAdLoadFailed(adUnitId: String?, errorCode: Int) { logCallback() }
+
+    override fun onAdHidden(ad: MaxAd?) { logCallback() }
+
+    override fun onAdDisplayFailed(ad: MaxAd?, errorCode: Int) { logCallback() }
+
+    override fun onAdDisplayed(ad: MaxAd?) { logCallback() }
+
+    override fun onAdClicked(ad: MaxAd?) { logCallback() }
+
+    override fun onAdExpanded(ad: MaxAd?) { logCallback() }
+
+    override fun onAdCollapsed(ad: MaxAd?) { logCallback() }
+
+    //endregion
+}
