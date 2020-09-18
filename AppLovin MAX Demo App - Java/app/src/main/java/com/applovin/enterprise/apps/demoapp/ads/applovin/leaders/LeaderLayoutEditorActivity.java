d69c345ba096740cae0fef1ec4b1b5a2b62629c1
Varsha Hanji <varsha.hanji@applovin.com>
2020-09-17 18:52:00 -0700

    Leader Changes

--- AppLovin MAX Demo App - Java/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/applovin/leaders/LeaderLayoutEditorActivity.java
+++ AppLovin MAX Demo App - Java/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/applovin/leaders/LeaderLayoutEditorActivity.java
@@ -7,13 +7,15 @@ import com.applovin.adview.AppLovinAdView;
 import com.applovin.adview.AppLovinAdViewDisplayErrorCode;
 import com.applovin.adview.AppLovinAdViewEventListener;
 import com.applovin.enterprise.apps.demoapp.R;
-import com.applovin.enterprise.apps.demoapp.ads.applovin.AdStatusActivity;
+import com.applovin.enterprise.apps.demoapp.ui.BaseAdActivity;
 import com.applovin.sdk.AppLovinAd;
+import com.applovin.sdk.AppLovinAdClickListener;
 import com.applovin.sdk.AppLovinAdDisplayListener;
 import com.applovin.sdk.AppLovinAdLoadListener;
 
 public class LeaderLayoutEditorActivity
-        extends AdStatusActivity
+        extends BaseAdActivity
+        implements AppLovinAdLoadListener, AppLovinAdDisplayListener, AppLovinAdViewEventListener, AppLovinAdClickListener
 {
     @Override
     protected void onCreate(final Bundle savedInstanceState)
@@ -21,88 +23,59 @@ public class LeaderLayoutEditorActivity
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_leader_layout_editor );
 
-        adStatusTextView = findViewById( R.id.status_label );
+        setupCallbacksRecyclerView();
 
         AppLovinAdView adView = findViewById( R.id.ad_view );
+        adView.setAdLoadListener( this );
+        adView.setAdDisplayListener( this );
+        adView.setAdViewEventListener( this );
+        adView.setAdClickListener( this );
 
         Button loadButton = findViewById( R.id.load_button );
         loadButton.setOnClickListener( view -> {
-            log( "Loading ad..." );
             adView.loadNextAd();
-        } );
-
-        //
-        // Optional: Set listeners
-        //
-        adView.setAdLoadListener( new AppLovinAdLoadListener()
-        {
-            @Override
-            public void adReceived(final AppLovinAd ad)
-            {
-                log( "Leader loaded" );
-            }
-
-            @Override
-            public void failedToReceiveAd(final int errorCode)
-            {
-                // Look at AppLovinErrorCodes.java for list of error codes.
-                log( "Leader failed to load with error code " + errorCode );
-            }
-        } );
-
-        adView.setAdDisplayListener( new AppLovinAdDisplayListener()
-        {
-            @Override
-            public void adDisplayed(final AppLovinAd ad)
-            {
-                log( "Leader displayed" );
-            }
-
-            @Override
-            public void adHidden(final AppLovinAd ad)
-            {
-                log( "Leader hidden" );
-            }
-        } );
-
-        adView.setAdClickListener( ad -> log( "Leader clicked" ) );
-
-        adView.setAdViewEventListener( new AppLovinAdViewEventListener()
-        {
-            @Override
-            public void adOpenedFullscreen(final AppLovinAd ad, final AppLovinAdView adView)
-            {
-                log( "Leader opened fullscreen" );
-            }
-
-            @Override
-            public void adClosedFullscreen(final AppLovinAd ad, final AppLovinAdView adView)
-            {
-                log( "Leader closed fullscreen" );
-            }
-
-            @Override
-            public void adLeftApplication(final AppLovinAd ad, final AppLovinAdView adView)
-            {
-                log( "Leader left application" );
-            }
-
-            @Override
-            public void adFailedToDisplay(final AppLovinAd ad, final AppLovinAdView adView, final AppLovinAdViewDisplayErrorCode code)
-            {
-                log( "Leader failed to display with error code " + code );
-            }
         } );
 
         // Load an ad!
         adView.loadNextAd();
 
-        //
-        // Please note that the AppLovinAdView CAN AUTOMATICALLY invoke loadNextAd() upon inflation from layout
-        // To do so, add the following attributes to the com.applovin.adview.AppLovinAdView element:
-        //
-        // xmlns:demo="http://schemas.applovin.com/android/1.0"
-        // demo:loadAdOnCreate="true"
-        //
     }
+
+    //region AppLovin Ad Load Listener
+
+    @Override
+    public void adReceived(final AppLovinAd ad)
+    {
+        logCallback();
+    }
+
+    @Override
+    public void failedToReceiveAd(final int errorCode) { logCallback(); }
+
+    //region Ad Display Load Listener
+
+    @Override
+    public void adDisplayed(final AppLovinAd ad) { logCallback(); }
+
+    @Override
+    public void adHidden(final AppLovinAd ad) { logCallback(); }
+
+    //region AdView Event Listener
+
+    @Override
+    public void adOpenedFullscreen(final AppLovinAd ad, final AppLovinAdView adView) { logCallback(); }
+
+    @Override
+    public void adClosedFullscreen(final AppLovinAd ad, final AppLovinAdView adView) { logCallback(); }
+
+    @Override
+    public void adLeftApplication(final AppLovinAd ad, final AppLovinAdView adView) { logCallback(); }
+
+    @Override
+    public void adFailedToDisplay(final AppLovinAd ad, final AppLovinAdView adView, final AppLovinAdViewDisplayErrorCode code) { logCallback(); }
+
+    //region Ad Click Listener
+
+    @Override
+    public void adClicked(final AppLovinAd ad) { logCallback(); }
 }
