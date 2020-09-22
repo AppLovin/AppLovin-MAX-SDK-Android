3cddc6804b0efe81783dd8c3c5fe06442c9bb60a
Varsha Hanji <varsha.hanji@applovin.com>
2020-09-21 17:00:02 -0700

    Interstitial-Kotlin changes

--- AppLovin MAX Demo App - Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/applovin/interstitials/InterstitialBasicIntegrationActivity.kt
+++ AppLovin MAX Demo App - Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/applovin/interstitials/InterstitialBasicIntegrationActivity.kt
@@ -2,93 +2,76 @@ package com.applovin.enterprise.apps.demoapp.ads.applovin.interstitials
 
 import android.os.Bundle
 import com.applovin.adview.AppLovinInterstitialAd
-import com.applovin.enterprise.apps.demoapp.ads.AdStatusActivity
-
-import com.applovin.sdk.AppLovinAd
-import com.applovin.sdk.AppLovinAdDisplayListener
-import com.applovin.sdk.AppLovinAdLoadListener
-import com.applovin.sdk.AppLovinAdClickListener
-import com.applovin.sdk.AppLovinAdVideoPlaybackListener
-import com.applovin.sdk.AppLovinSdk
-import kotlinx.android.synthetic.main.activity_interstitial_basic_integration.*
 import com.applovin.enterprise.apps.demoapp.R
+import com.applovin.enterprise.apps.demoapp.ui.BaseAdActivity
+import com.applovin.sdk.*
+import kotlinx.android.synthetic.main.activity_interstitial_basic_integration.*
 
-class InterstitialBasicIntegrationActivity : AdStatusActivity(), AppLovinAdLoadListener, AppLovinAdDisplayListener, AppLovinAdClickListener, AppLovinAdVideoPlaybackListener
+class InterstitialBasicIntegrationActivity : BaseAdActivity(), AppLovinAdLoadListener, AppLovinAdDisplayListener, AppLovinAdClickListener, AppLovinAdVideoPlaybackListener
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_interstitial_basic_integration)
 
-        adStatusTextView = status_label
+        setupCallbacksRecyclerView()
 
         val interstitialAdDialog = AppLovinInterstitialAd.create(AppLovinSdk.getInstance(this), this)
 
+        interstitialAdDialog.setAdLoadListener(this)
+        interstitialAdDialog.setAdDisplayListener(this)
+        interstitialAdDialog.setAdClickListener(this)
+        interstitialAdDialog.setAdVideoPlaybackListener(this) // This will only ever be used if you have video ads enabled.
+
         showButton.setOnClickListener {
             showButton.isEnabled = false
-
-            log("Showing...")
-
-            //
-            // Optional: Set ad load, ad display, ad click, and ad video playback callback listeners
-            //
-            interstitialAdDialog.setAdLoadListener(this)
-            interstitialAdDialog.setAdDisplayListener(this)
-            interstitialAdDialog.setAdClickListener(this)
-            interstitialAdDialog.setAdVideoPlaybackListener(this) // This will only ever be used if you have video ads enabled.
 
             interstitialAdDialog.show()
         }
     }
 
-    //
-    // Ad Load Listener
-    //
+    //region Ad Load Listener
+
     override fun adReceived(appLovinAd: AppLovinAd)
     {
-        log("Interstitial loaded")
+        logCallback()
         showButton.isEnabled = true
     }
 
     override fun failedToReceiveAd(errorCode: Int)
     {
         // Look at AppLovinErrorCodes.java for list of error codes
-        log("Interstitial failed to load with error code " + errorCode)
-
+        logCallback()
         showButton.isEnabled = true
     }
 
-    //
-    // Ad Display Listener
-    //
-    override fun adDisplayed(appLovinAd: AppLovinAd)
-    {
-        log("Interstitial Displayed")
-    }
+    //endregion
 
-    override fun adHidden(appLovinAd: AppLovinAd)
-    {
-        log("Interstitial Hidden")
-    }
+    //region Ad Display Listener
 
-    //
-    // Ad Click Listener
-    //
-    override fun adClicked(appLovinAd: AppLovinAd)
-    {
-        log("Interstitial Clicked")
-    }
+    override fun adDisplayed(appLovinAd: AppLovinAd) { logCallback() }
 
-    //
-    // Ad Video Playback Listener
-    //
+    override fun adHidden(appLovinAd: AppLovinAd) { logCallback() }
+
+    //endregion
+
+    //region Ad Click Listener
+
+    override fun adClicked(appLovinAd: AppLovinAd) { logCallback() }
+
+    //endregion
+
+    //region Ad Video Playback Listener
+
     override fun videoPlaybackBegan(appLovinAd: AppLovinAd)
     {
-        log("Video Started")
+        logCallback()
     }
 
     override fun videoPlaybackEnded(appLovinAd: AppLovinAd, percentViewed: Double, wasFullyViewed: Boolean)
     {
-        log("Video Ended")
+        logCallback()
     }
+
+    //endregion
 }
