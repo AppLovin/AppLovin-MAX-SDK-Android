85b97a57748e2883d4208cd20305206761d1e5b1
NanaAmoah <90287918+NanaAmoah@users.noreply.github.com>
2021-11-16 15:25:44 -0800

    Added MREC layout editor

--- AppLovin MAX Demo App - Java/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/applovin/mrecs/MrecDemoMenuActivity.java
+++ AppLovin MAX Demo App - Java/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/applovin/mrecs/MrecDemoMenuActivity.java
@@ -1,5 +1,20 @@
 package com.applovin.enterprise.apps.demoapp.ads.applovin.mrecs;
 
+import android.content.Intent;
+
+import com.applovin.enterprise.apps.demoapp.ads.applovin.DemoMenuActivity;
+import com.applovin.enterprise.apps.demoapp.data.main.DemoMenuItem;
+
 public class MrecDemoMenuActivity
+    extends DemoMenuActivity
 {
+    @Override
+    protected DemoMenuItem[] getListViewContents()
+    {
+        return new DemoMenuItem[] {
+                new DemoMenuItem( "Programmatic", new Intent( this, MrecProgrammaticActivity.class ) ),
+                new DemoMenuItem( "Layout Editor", new Intent( this, MrecLayoutEditorActivity.class ) ),
+                new DemoMenuItem( "Zone Integration", new Intent( this, MrecZoneActivity.class ) ),
+        };
+    }
 }
