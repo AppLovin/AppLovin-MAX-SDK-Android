60bbae23eb6f670c7b84693bc794506da713ffef
prod-applovin-deploy <dev@applovin.com>
2022-02-28 15:38:17 -0800

    UnityAds/4.0.1.0

--- UnityAds/CHANGELOG.md
+++ UnityAds/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 4.0.1.0
+* Certified with UnityAds SDK 4.0.1.
+
 ## 4.0.0.2
 * Support for null `Activity` on init.
 
--- UnityAds/build.gradle.kts
+++ UnityAds/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 
 private val versionMajor = 4
 private val versionMinor = 0
-private val versionPatch = 0
-private val versionAdapterPatch = 2
+private val versionPatch = 1
+private val versionAdapterPatch = 0
 
 val libraryVersionName by extra("${versionMajor}.${versionMinor}.${versionPatch}.${versionAdapterPatch}")
 val libraryVersionCode by extra((versionMajor * 1000000) + (versionMinor * 10000) + (versionPatch * 100) + versionAdapterPatch)
