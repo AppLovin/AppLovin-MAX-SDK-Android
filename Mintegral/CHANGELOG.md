cb86d11ee0e9b8a8f98afa6e635026353d9f11cf
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2022-04-15 20:27:17 -0700

    Mintegral/16.1.11.0

--- Mintegral/CHANGELOG.md
+++ Mintegral/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 16.1.11.0
+* Certified with Mintegral SDK 16.1.11.
+
 ## 16.0.31.2
 * Remove check for manual native ad assets.
 
--- Mintegral/build.gradle.kts
+++ Mintegral/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 
 // NOTE: Mintegral has 2 separate SDK versions, e.g. x.x.51 for Google Play & x.x.52 for Android Market (in China)
 private val versionMajor = 16
-private val versionMinor = 0
-private val versionPatch = 31
-private val versionAdapterPatch = 2
+private val versionMinor = 1
+private val versionPatch = 11
+private val versionAdapterPatch = 0
 
 val libraryVersionName by extra("${versionMajor}.${versionMinor}.${versionPatch}.${versionAdapterPatch}")
 val libraryVersionCode by extra((versionMajor * 1000000) + (versionMinor * 10000) + (versionPatch * 100) + versionAdapterPatch)
