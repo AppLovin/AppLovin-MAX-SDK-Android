28123a8a30b92c0205427e56ce8de38db1349221
Ritam Sarmah <ritamsarmah@users.noreply.github.com>
2022-05-25 09:06:50 -0700

    feat/ad_placer (#338)
    * Add ad placer to Java demo app
    
    * Change recycler adapter type
    
    * Add ad placer to Kotlin demo app and enable multidex
    
    * Change repeating interval to better suit sample data
    
    * Fix data splitting
    
    * Apply suggestions from code review
    
    * Update AppLovin MAX Demo App - Java/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/max/nativead/RecyclerViewNativeAdActivity.java
    
    * Update AppLovin MAX Demo App - Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/max/nativead/RecyclerViewNativeAdActivity.kt
    
    Co-authored-by: Santosh Bagadi <37196391+santoshbagadi@users.noreply.github.com>

--- AppLovin MAX Demo App - Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/max/nativead/NativeAdActivity.kt
+++ AppLovin MAX Demo App - Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/ads/max/nativead/NativeAdActivity.kt
@@ -6,7 +6,9 @@ import com.applovin.enterprise.apps.demoapp.data.main.DemoMenuItem
 
 class NativeAdActivity : DemoMenuActivity() {
     override fun getListViewContents(): Array<DemoMenuItem> = arrayOf(
-            DemoMenuItem("Templates API", Intent(this, TemplateNativeAdActivity::class.java)),
-            DemoMenuItem("Manual API", Intent(this, ManualNativeAdActivity::class.java)),
-            DemoMenuItem("Manual Late Binding API", Intent(this, ManualNativeLateBindingAdActivity::class.java)))
+        DemoMenuItem("Templates API", Intent(this, TemplateNativeAdActivity::class.java)),
+        DemoMenuItem("Manual API", Intent(this, ManualNativeAdActivity::class.java)),
+        DemoMenuItem("Manual Late Binding API", Intent(this, ManualNativeLateBindingAdActivity::class.java)),
+        DemoMenuItem("Recycler View Ad Placer", Intent(this, RecyclerViewNativeAdActivity::class.java))
+    )
 }
