e0c3b89f34774a7d5e3e091168e8ac88d4d3c813
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2022-10-12 04:10:50 -0700

    ByteDance/4.7.0.7.0

--- ByteDance/CHANGELOG.md
+++ ByteDance/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 4.7.0.7.0
+* Certified with ByteDance SDK 4.7.0.7.
+
 ## 4.7.0.6.0
 * Certified with ByteDance SDK 4.7.0.6.
 
--- ByteDance/build.gradle.kts
+++ ByteDance/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 private val versionMajor = 4
 private val versionMinor = 7
 private val versionPatch = 0
-private val versionBuild = 6
+private val versionBuild = 7
 private val versionAdapterPatch = 0
 
 val libraryVersionName by extra("${versionMajor}.${versionMinor}.${versionPatch}.${versionBuild}.${versionAdapterPatch}")
