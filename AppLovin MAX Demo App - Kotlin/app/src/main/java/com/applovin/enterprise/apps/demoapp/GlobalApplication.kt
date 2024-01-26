6fddb7bf52cf21321b679c0356fc1a32bb018953
Harry Arakkal <harry.arakkal@applovin.com>
2024-01-25 19:10:44 -0500

    config/add_default_sdk_key (#619)

--- AppLovin MAX Demo App - Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/GlobalApplication.kt
+++ AppLovin MAX Demo App - Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/GlobalApplication.kt
@@ -5,9 +5,12 @@ import android.app.Application
 import android.os.Bundle
 import com.adjust.sdk.Adjust
 import com.adjust.sdk.AdjustConfig
-import com.adjust.sdk.LogLevel
 import com.applovin.sdk.AppLovinMediationProvider
 import com.applovin.sdk.AppLovinSdk
+import com.applovin.sdk.AppLovinSdkSettings
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import java.util.Collections
+import java.util.concurrent.Executors
 
 
 class GlobalApplication : Application() {
@@ -15,16 +18,33 @@ class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // Initialize the AppLovin SDK
-        AppLovinSdk.getInstance(this).mediationProvider = AppLovinMediationProvider.MAX
-        AppLovinSdk.getInstance(this).initializeSdk {
-            // AppLovin SDK is initialized, start loading ads now or later if ad gate is reached
+        // If you want to test your own AppLovin SDK key,
+        // update the value in AndroidManifest.xml under the "applovin.sdk.key" key, and update the package name to your app's name.
 
-            // Initialize Adjust SDK
-            val config = AdjustConfig(this, "{YourAppToken}", AdjustConfig.ENVIRONMENT_SANDBOX)
-            Adjust.onCreate(config)
+        val executor = Executors.newSingleThreadExecutor();
+        executor.execute {
+            // Enable test mode by default for the current device. Cannot be run on the main thread.
+            val currentGaid = AdvertisingIdClient.getAdvertisingIdInfo(this).id
 
-            registerActivityLifecycleCallbacks(AdjustLifecycleCallbacks())
+            val settings = AppLovinSdkSettings(this)
+            if (currentGaid != null) {
+                settings.testDeviceAdvertisingIds = Collections.singletonList(currentGaid)
+            }
+
+            // Initialize the AppLovin SDK
+            val sdk = AppLovinSdk.getInstance(settings, this)
+            sdk.mediationProvider = AppLovinMediationProvider.MAX
+            sdk.initializeSdk {
+                // AppLovin SDK is initialized, start loading ads now or later if ad gate is reached
+
+                // Initialize Adjust SDK
+                val config = AdjustConfig(this, "{YourAppToken}", AdjustConfig.ENVIRONMENT_SANDBOX)
+                Adjust.onCreate(config)
+
+                registerActivityLifecycleCallbacks(AdjustLifecycleCallbacks())
+            }
+
+            executor.shutdown()
         }
     }
 
