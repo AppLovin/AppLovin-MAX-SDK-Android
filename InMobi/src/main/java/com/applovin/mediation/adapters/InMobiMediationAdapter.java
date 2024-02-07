5075171b03d0e31360b4db52ae58f469000381f4
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-02-07 10:55:20 -0800

    InMobi/10.6.3.1

--- InMobi/src/main/java/com/applovin/mediation/adapters/InMobiMediationAdapter.java
+++ InMobi/src/main/java/com/applovin/mediation/adapters/InMobiMediationAdapter.java
@@ -1296,10 +1296,12 @@ public class InMobiMediationAdapter
                         primaryViewWidth = (int) ( primaryViewHeight * getMediaContentAspectRatio() );
                     }
 
+                    ViewGroup.LayoutParams layoutParams = mediaView.getLayoutParams();
+                    
                     // Compute primaryViewWidth when it is a dynamic layout value before getting the actual measurement.
-                    if ( primaryViewWidth == 0 )
+                    if ( primaryViewWidth == 0 && layoutParams != null )
                     {
-                        int layoutWidth = mediaView.getLayoutParams().width;
+                        int layoutWidth = layoutParams.width;
                         if ( layoutWidth == ViewGroup.LayoutParams.WRAP_CONTENT || layoutWidth == ViewGroup.LayoutParams.MATCH_PARENT )
                         {
                             primaryViewWidth = (int) ( primaryViewHeight * getMediaContentAspectRatio() );
