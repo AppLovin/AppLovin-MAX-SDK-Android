51b32e25215c73cd61faf37237d4cf059e816efa
Paul Hounshell <paul.hounshell@applovin.com>
2024-04-10 10:01:17 -0700

    config/gradle_8.6 (#655)
    Patch up demo build files and use a better technique for detecting
    whether the projects are being run as part of the AppLovin workspace or
    as a standalone demo.

--- AppLovin MAX Demo App - Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/data/main/Footer.kt
+++ AppLovin MAX Demo App - Kotlin/app/src/main/java/com/applovin/enterprise/apps/demoapp/data/main/Footer.kt
@@ -1,7 +1,6 @@
 package com.applovin.enterprise.apps.demoapp.data.main
 
 import android.os.Build
-import com.applovin.enterprise.apps.demoapp.BuildConfig
 import com.applovin.sdk.AppLovinSdk
 
 
@@ -18,7 +17,7 @@ data class Footer(override val type: Int = ListItem.FOOTER) : ListItem
         val fields = Build.VERSION_CODES::class.java.fields
         var versionName = "UNKNOWN"
         fields.filter { it.getInt(Build.VERSION_CODES::class) == Build.VERSION.SDK_INT }
-                .forEach { versionName = it.name }
+            .forEach { versionName = it.name }
         val apiLevel = Build.VERSION.SDK_INT
 
         return """
