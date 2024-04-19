9e4dfae5b9ac56465fe6d09117ab7e6e4fc2c1fc
jenkins <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-04-19 07:11:45 -0700

    MobileFuse/1.7.4.0

--- MobileFuse/CHANGELOG.md
+++ MobileFuse/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 1.7.4.0
+* Certified with MobileFuse SDK 1.7.4.
+
 ## 1.7.3.0
 * Certified with MobileFuse SDK 1.7.3.
 
--- MobileFuse/build.gradle.kts
+++ MobileFuse/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 private val versionMajor = 1
 private val versionMinor = 7
-private val versionPatch = 3
+private val versionPatch = 4
 private val versionAdapterPatch = 0
 
 val libraryVersionName by extra("${versionMajor}.${versionMinor}.${versionPatch}.${versionAdapterPatch}")
@@ -16,8 +16,10 @@ val libraryGroupId by extra("com.applovin.mediation")
 
 var libraryVersions = rootProject.extra["versions"] as Map<*, *>
 
+android.namespace = "com.applovin.mediation.adapters.mobilefuse"
 android.defaultConfig.versionCode = libraryVersionCode
 android.defaultConfig.versionName = libraryVersionName
+android.defaultConfig.minSdk = 19
 
 dependencies {
     implementation("com.mobilefuse.sdk:mobilefuse-sdk-core:${libraryVersions["mobileFuse"]}")
