a9cefc9585d585f54c6f0bed4c171d83ecfa59f1
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-06-13 04:08:18 -0700

    Mintegral/16.7.61.0

--- Mintegral/CHANGELOG.md
+++ Mintegral/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 16.7.61.0
+* Certified with Mintegral SDK 16.7.61.
+
 ## 16.7.51.0
 * Certified with Mintegral SDK 16.7.51.
 
--- Mintegral/build.gradle.kts
+++ Mintegral/build.gradle.kts
@@ -7,7 +7,7 @@ afterEvaluate {
 }
 
 // NOTE: Mintegral has 2 separate SDK versions, e.g. x.x.51 for Google Play & x.x.52 for Android Market (in China)
-val libraryVersionName by extra("16.7.51.0")
+val libraryVersionName by extra("16.7.61.0")
 
 repositories {
     maven { url = uri("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea") }
