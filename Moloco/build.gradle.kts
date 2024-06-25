d4bf4395d339191bde46898f974b152d556ef91e
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-06-25 15:33:44 -0700

    Moloco/3.0.1.0

--- Moloco/build.gradle.kts
+++ Moloco/build.gradle.kts
@@ -6,6 +6,6 @@ afterEvaluate {
     apply(plugin = "adapter-publish")
 }
 
-val libraryVersionName by extra("3.0.0.0")
+val libraryVersionName by extra("3.0.1.0")
 
 android.defaultConfig.minSdk = 21
