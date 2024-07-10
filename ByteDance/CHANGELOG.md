49e670331255596b04a67943a4d929e1798b3320
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-07-10 04:11:15 -0700

    ByteDance/5.9.1.1.0

--- ByteDance/CHANGELOG.md
+++ ByteDance/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 5.9.1.1.0
+* Certified with ByteDance SDK 5.9.1.1.
+
 ## 5.9.0.6.0
 * Certified with ByteDance SDK 5.9.0.6.
 
--- ByteDance/build.gradle.kts
+++ ByteDance/build.gradle.kts
@@ -6,7 +6,7 @@ afterEvaluate {
     apply(plugin = "adapter-publish")
 }
 
-val libraryVersionName by extra("5.9.0.6.0")
+val libraryVersionName by extra("5.9.1.1.0")
 
 repositories {
     maven { url = uri("https://artifact.bytedance.com/repository/pangle") }
