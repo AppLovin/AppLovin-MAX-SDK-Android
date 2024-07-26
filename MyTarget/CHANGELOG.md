fc26907b92d73c52b79110433415a2ff12588bb4
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-07-26 10:09:27 -0700

    MyTarget/5.22.1.0

--- MyTarget/CHANGELOG.md
+++ MyTarget/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 5.22.1.0
+* Certified with MyTarget SDK 5.22.1.
+
 ## 5.22.0.0
 * Certified with MyTarget SDK 5.22.0.
 
--- MyTarget/build.gradle.kts
+++ MyTarget/build.gradle.kts
@@ -6,7 +6,7 @@ afterEvaluate {
     apply(plugin = "adapter-publish")
 }
 
-val libraryVersionName by extra("5.22.0.0")
+val libraryVersionName by extra("5.22.1.0")
 
 android.defaultConfig.minSdk = 21
 
