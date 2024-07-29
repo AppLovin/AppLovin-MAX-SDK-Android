a8d2a33ddb9e16102938dbd6fd308e3c795aa22d
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2024-07-29 09:02:32 -0700

    BidMachine/3.0.0.0

--- BidMachine/build.gradle.kts
+++ BidMachine/build.gradle.kts
@@ -1,63 +1,15 @@
-import com.applovin.build.extensions.appendDependencyBundle
-
 plugins {
-    id("signing")
-    id("maven-publish")
+    id("adapter-config")
 }
 
-private val versionMajor = 2
-private val versionMinor = 7
-private val versionPatch = 0
-private val versionAdapterPatch = 0
+afterEvaluate {
+    apply(plugin = "adapter-publish")
+}
 
-val libraryVersionName by extra("${versionMajor}.${versionMinor}.${versionPatch}.${versionAdapterPatch}")
-val libraryVersionCode by extra((versionMajor * 1000000) + (versionMinor * 10000) + (versionPatch * 100) + versionAdapterPatch)
+val libraryVersionName by extra("3.0.0.0")
 
-val libraryArtifactId by extra("bidmachine-adapter")
-val libraryGroupId by extra("com.applovin.mediation")
-
-android.namespace = "com.applovin.mediation.adapters.bidmachine"
-android.defaultConfig.versionCode = libraryVersionCode
-android.defaultConfig.versionName = libraryVersionName
+android.defaultConfig.minSdk = 21
 
 repositories {
     maven { url = uri("https://artifactory.bidmachine.io/bidmachine") }
-}
-
-dependencies {
-    implementation(mediation.bundles.bidmachine)
-    compileOnly(libs.android.annotations)
-}
-
-publishing {
-    publications {
-        create<MavenPublication>(extra["publicationName"] as String) {
-            // The publication doesn't know about our dependencies, so we have to manually add them to the pom
-            pom.withXml {
-                asNode().apply {
-                    appendNode("name", libraryArtifactId)
-                    appendNode("description", "BidMachine adapter for AppLovin MAX mediation")
-                    appendNode("url", "https://www.applovin.com/")
-                    appendNode("licenses")
-                            .appendNode("license").apply {
-                                appendNode("name", "AppLovin Corporation Mediation Adapter EULA")
-                                appendNode("url", "https://www.applovin.com/eula")
-                            }
-                    appendNode("scm").apply {
-                        appendNode("connection", "scm:git:github.com/AppLovin/AppLovin-MAX-SDK-Android.git")
-                        appendNode("developerConnection", "scm:git:ssh://github.com/AppLovin/AppLovin-MAX-SDK-Android.git")
-                        appendNode("url", "https://github.com/AppLovin/AppLovin-MAX-SDK-Android")
-                    }
-                    appendNode("developers")
-                            .appendNode("developer").apply {
-                                appendNode("name", "AppLovin")
-                                appendNode("url", "https://www.applovin.com")
-                            }
-                    // Add BidMachine network to list of dependencies.
-                    appendNode("dependencies")
-                        .appendDependencyBundle(mediation.bundles.bidmachine)
-                }
-            }
-        }
-    }
 }
