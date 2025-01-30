4f9b19243cd20fa2ccf66578a3d37bd0fe0c304f
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-01-29 16:28:21 -0800

    GoogleAdManager/23.6.0.3

--- GoogleAdManager/build.gradle.kts
+++ GoogleAdManager/build.gradle.kts
@@ -6,7 +6,7 @@ afterEvaluate {
     apply(plugin = "adapter-publish")
 }
 
-val libraryVersionName by extra("23.6.0.2")
+val libraryVersionName by extra("23.6.0.3")
 
 val libraryArtifactId by extra("google-ad-manager-adapter")
 
