fb4806443f7391b8619a4b467dd7dc448abe35fe
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-01-29 17:32:45 -0800

    PubMatic/4.3.0.1

--- PubMatic/build.gradle.kts
+++ PubMatic/build.gradle.kts
@@ -6,7 +6,7 @@ afterEvaluate {
     apply(plugin = "adapter-publish")
 }
 
-val libraryVersionName by extra("4.3.0.0")
+val libraryVersionName by extra("4.3.0.1")
 
 android.defaultConfig.minSdk = 19
 
