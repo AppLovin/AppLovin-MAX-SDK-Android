2ee0ff643758ee6be1547a83dc8b6b731b0b88ed
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-05-05 06:08:04 -0700

    Yandex/7.12.2.0

--- Yandex/build.gradle.kts
+++ Yandex/build.gradle.kts
@@ -6,7 +6,7 @@ afterEvaluate {
     apply(plugin = "adapter-publish")
 }
 
-val libraryVersionName by extra("7.12.1.1")
+val libraryVersionName by extra("7.12.2.0")
 val minAppLovinSdkVersion by extra("13.0.0")
 
 android.defaultConfig.minSdk = 21
