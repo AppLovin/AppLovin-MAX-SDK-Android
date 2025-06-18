6f5e493532f5d5b3165dc0d01cef59a521f38fdb
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-06-18 06:29:11 -0700

    BidMachine/3.3.0.1

--- BidMachine/CHANGELOG.md
+++ BidMachine/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 3.3.0.1
+* Updated ad display failed error code.
+
 ## 3.3.0.0
 * Certified with BidMachine SDK 3.3.0.
 
--- BidMachine/build.gradle.kts
+++ BidMachine/build.gradle.kts
@@ -6,7 +6,7 @@ afterEvaluate {
     apply(plugin = "adapter-publish")
 }
 
-val libraryVersionName by extra("3.3.0.0")
+val libraryVersionName by extra("3.3.0.1")
 val minAppLovinSdkVersion by extra("13.0.0")
 
 android.defaultConfig.minSdk = 21
