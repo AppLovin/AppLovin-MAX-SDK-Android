e60c1afeb6bcafbdea752977358381916e1b23cd
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-09-23 17:24:03 -0700

    Chartboost/9.10.0.0

--- Chartboost/build.gradle.kts
+++ Chartboost/build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
     id("adapter-config")
+    id("com.applovin.mobile.publish")
 }
 
-afterEvaluate {
-    apply(plugin = "adapter-publish")
-}
-
-val libraryVersionName by extra("9.9.3.0")
+val libraryVersionName by extra("9.10.0.0")
 val minAppLovinSdkVersion by extra("13.0.0")
 
 android.defaultConfig.minSdk = 19
