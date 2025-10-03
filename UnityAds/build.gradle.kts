f93cd37445fa19f64a31ce63dcb4ad4affab96c0
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-10-03 12:15:35 -0700

    UnityAds/4.16.2.0

--- UnityAds/build.gradle.kts
+++ UnityAds/build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
     id("adapter-config")
+    id("com.applovin.mobile.publish")
 }
 
-afterEvaluate {
-    apply(plugin = "adapter-publish")
-}
-
-val libraryVersionName by extra("4.16.1.0")
+val libraryVersionName by extra("4.16.2.0")
 val minAppLovinSdkVersion by extra("13.0.0")
 
 android.defaultConfig.minSdk = 19
