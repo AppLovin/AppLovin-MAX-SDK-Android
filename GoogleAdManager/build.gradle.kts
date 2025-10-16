04f38af1746b8985ff549ac6a7dfe25fcfadcb44
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-10-16 14:01:47 -0700

    GoogleAdManager/24.7.0.0

--- GoogleAdManager/build.gradle.kts
+++ GoogleAdManager/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.applovin.mobile.publish")
 }
 
-val libraryVersionName by extra("24.6.0.0")
+val libraryVersionName by extra("24.7.0.0")
 val minAppLovinSdkVersion by extra("13.0.0")
 
 applovinMobilePublish {
