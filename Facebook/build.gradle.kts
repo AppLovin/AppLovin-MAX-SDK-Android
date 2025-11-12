dfeb33de18daa0acedb8a85089bcba9b9d35901f
LugaWang <luga.wang@applovin.com>
2025-11-12 10:50:47 -0800

    bump version

--- Facebook/build.gradle.kts
+++ Facebook/build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
     id("adapter-config")
+    id("com.applovin.mobile.publish")
 }
 
-afterEvaluate {
-    apply(plugin = "adapter-publish")
-}
-
-val libraryVersionName by extra("6.20.0.0")
+val libraryVersionName by extra("6.21.0.0")
 val minAppLovinSdkVersion by extra("13.0.0")
 
 android.defaultConfig.minSdk = 16
