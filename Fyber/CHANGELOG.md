9bb7b50453a2cc5b3cb0dedc8f4138b414239998
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-11-18 08:11:22 -0800

    Fyber/8.4.1.0

--- Fyber/CHANGELOG.md
+++ Fyber/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 8.4.1.0
+* Certified with Fyber SDK 8.4.1.
+
 ## 8.4.0.0
 * Certified with Fyber SDK 8.4.0.
 
--- Fyber/build.gradle.kts
+++ Fyber/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.applovin.mobile.publish")
 }
 
-val libraryVersionName by extra("8.4.0.0")
+val libraryVersionName by extra("8.4.1.0")
 val minAppLovinSdkVersion by extra("13.2.0")
 
 android.defaultConfig.minSdk = 19
