b2984c5d9fa02298a47c0fdefec03eb8fc1dfeef
Akifumi Shinagawa <ashinagawa@liftoff.io>
2026-02-12 12:55:14 -0800

    Set adapterAdFormat to ad objects

--- Vungle/src/main/java/com/applovin/mediation/adapters/VungleMediationAdapter.java
+++ Vungle/src/main/java/com/applovin/mediation/adapters/VungleMediationAdapter.java
@@ -219,7 +219,8 @@ public class VungleMediationAdapter
 
         interstitialAd = new InterstitialAd( getContext( activity ), placementId, new AdConfig() );
         interstitialAd.setAdListener( new InterstitialListener( listener ) );
-
+        interstitialAd.setAdapterAdFormat("MaxInterstitialAdapter");
+        
         interstitialAd.load( bidResponse );
     }
 
@@ -263,6 +264,7 @@ public class VungleMediationAdapter
 
         appOpenAd = new InterstitialAd( getContext( activity ), placementId, new AdConfig() );
         appOpenAd.setAdListener( new AppOpenAdListener( listener ) );
+        appOpenAd.setAdapterAdFormat("MaxAppOpenAdapter");
 
         appOpenAd.load( bidResponse );
     }
@@ -307,6 +309,7 @@ public class VungleMediationAdapter
 
         rewardedAd = new RewardedAd( getContext( activity ), placementId, new AdConfig() );
         rewardedAd.setAdListener( new RewardedListener( listener ) );
+        rewardedAd.setAdapterAdFormat("MaxRewardedAdapter");
 
         rewardedAd.load( bidResponse );
     }
@@ -362,6 +365,7 @@ public class VungleMediationAdapter
             final NativeAdViewListener nativeAdViewListener = new NativeAdViewListener( parameters, adFormat, context, listener );
             nativeAd = new NativeAd( getContext( activity ), placementId );
             nativeAd.setAdListener( nativeAdViewListener );
+            nativeAd.setAdapterAdFormat("MaxAdViewAdapter-Native");
 
             nativeAd.load( bidResponse );
 
@@ -379,6 +383,7 @@ public class VungleMediationAdapter
         VungleAdSize adSize = toVungleAdSize( adFormat, isAdaptiveAdViewEnabled, parameters, context );
         adViewAd = new VungleBannerView( context, placementId, adSize );
         adViewAd.setAdListener( new AdViewAdListener( adFormatLabel, listener ) );
+        adViewAd.setAdapterAdFormat("MaxAdViewAdapter");
 
         adViewAd.load( bidResponse );
     }
@@ -407,6 +412,7 @@ public class VungleMediationAdapter
 
         nativeAd = new NativeAd( getContext( activity ), placementId );
         nativeAd.setAdListener( new NativeListener( parameters, getContext( activity ), listener ) );
+        nativeAd.setAdapterAdFormat("MaxNativeAdAdapter");
 
         nativeAd.load( bidResponse );
     }
