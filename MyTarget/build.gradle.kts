df4c1bd332f577bebfb24bcc5973267cee63a3c4
LugaWang <luga.wang@applovin.com>
2026-07-31 15:08:35 -0700

    ref/deprecate_mytarget (#935)

--- MyTarget/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("adapter-config")
-    id("com.applovin.mobile.publish")
-}
-
-val libraryVersionName by extra("5.47.1.0")
-val minAppLovinSdkVersion by extra("13.0.0")
-
-android.defaultConfig.minSdk = 21
