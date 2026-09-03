d417d328100efa72ae4b44448c9b84af9ed5a56d
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2026-09-03 06:18:27 -0700

    ByteDance/8.3.0.3.0

--- ByteDance/CHANGELOG.md
+++ ByteDance/CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 8.3.0.3.0
+* Certified with ByteDance SDK 8.3.0.3.
+
 ## 8.2.0.4.0
 * Certified with ByteDance SDK 8.2.0.4.
 
--- ByteDance/build.gradle.kts
+++ ByteDance/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.applovin.mobile.publish")
 }
 
-val libraryVersionName by extra("8.2.0.4.0")
+val libraryVersionName by extra("8.3.0.3.0")
 val minAppLovinSdkVersion by extra("13.2.0")
 
 android.defaultConfig.minSdk = 19
