9550847cb428fa407de10cbd040fc30556127b58
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2026-09-10 21:17:54 -0700

    AmazonAdMarketplace/12.0.2.0

--- AmazonAdMarketplace/build.gradle.kts
+++ AmazonAdMarketplace/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.applovin.mobile.publish")
 }
 
-val libraryVersionName by extra("12.0.1.0")
+val libraryVersionName by extra("12.0.2.0")
 
 applovinMobilePublish {
     libraryArtifactId.set("amazon-tam-adapter")
